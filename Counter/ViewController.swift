a7a5b9a4ac5b9614faf062029dbaf36d87a258e5
shurajan <shurajangit@gmail.com>
2024-04-22 15:12:16 +0300

    Спринт 3 - решение базовой задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var counter = 0
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "0"
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
 }
 
